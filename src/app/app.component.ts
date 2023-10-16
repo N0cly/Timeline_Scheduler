05c36573475c75cf1341be498131d6cf95af0092
Nocly <45365883+N0cly@users.noreply.github.com>
2023-10-16 21:42:56 +0200

    display element order by father_id w/ color red if code === '1'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { BryntumSchedulerProComponent, BryntumProjectModelComponent } from '@bryntum/schedulerpro-angular';
 import { schedulerProConfig, projectConfig } from './app.config';
-import {SchedulerPro, Store} from "@bryntum/schedulerpro";
 import logs from '../assets/data/talend_logs.json'
 
 @Component({
@@ -11,123 +10,62 @@ import logs from '../assets/data/talend_logs.json'
 })
 
 export class AppComponent {
-
-  // scheduler = new SchedulerPro({
-
-  //
-  //   appendTo : document.body,
-  //
-  //   // width : document.body.clientWidth,
-  //   // height : document.body.clientHeight,
-  //
-  //   startDate : '2022-01-01 01:00:00',
-  //   endDate   : '2022-01-01 23:59:59',
-  //
-  //   columns : [
-  //     {
-  //       field : 'name',
-  //       text  : 'Name'
-  //     }, {
-  //       field : 'role',
-  //       text  : 'Role'
-  //     }
-  //   ],
-  //
-  //   project : {
-  //     loadUrl  : 'http://localhost:3000/project',
-  //     autoLoad : true
-  //   }
-  //
-  // })
-
-
   inputData: any;
   rowsArray: any[] = [];
-  ressourcesArray: any[] = [];
-  assignementArray: any[] = [];
+  resourcesArray: any[] = [];
+  assignmentsArray: any[] = [];
+
   constructor() {
     this.inputData = logs;
+    const groupedData: any = {};
 
-    for (const record of this.inputData.RECORDS){
-      const newRessource = {
-        "id" : record.id,
-        "name": record.father_pid
-      }
-      // console.log(newRessource)
-      this.ressourcesArray.push(newRessource)
-    }
-     console.log(this.ressourcesArray)
 
     for (const record of this.inputData.RECORDS) {
-      const newRow = {
-        "id": record.id,
-        "startDate": record.moment,
-        "endDate": record.moment,
-        "name": record.father_pid
-      };
-      // console.log(newRow)
-      this.rowsArray.push(newRow);
+      const fatherPid = record.father_pid;
+
+      if (!groupedData[fatherPid]) {
+
+        groupedData[fatherPid] = {
+          startDate: record.moment,
+          endDate: record.moment,
+          eventColor: 'green'
+        };
+      } else {
+
+        groupedData[fatherPid].endDate = record.moment;
+      }
+
+      if (record.code === '1') {
+        groupedData[fatherPid].eventColor = 'red';
+      }
     }
-     console.log(this.rowsArray)
 
+    for (const fatherPid in groupedData) {
+      if (groupedData.hasOwnProperty(fatherPid)) {
+        this.resourcesArray.push({
+          id: fatherPid,
+          name: fatherPid
+        });
 
-    for (const record of this.inputData.RECORDS){
-      const newAssignement = {
-        "event": record.id,
-        "resource": record.id
-      };
-      // console.log(newAssignement)
-      this.assignementArray.push(newAssignement)
+        this.rowsArray.push({
+          id: fatherPid,
+          startDate: groupedData[fatherPid].startDate,
+          endDate: groupedData[fatherPid].endDate,
+          name: fatherPid,
+          eventColor: groupedData[fatherPid].eventColor
+        });
+
+        this.assignmentsArray.push({
+          event: fatherPid,
+          resource: fatherPid
+        });
+      }
     }
-     console.log(this.assignementArray)
-
-    // const outputData = {
-    //   events: {
-    //     rows: this.rowsArray
-    //   }
-    // };
-
-    // console.log(typeof outputData)
-
-    // const outputJson = JSON.stringify(outputData, null, 2);
-
-    //console.log(typeof outputJson)
   }
 
-  resources = this.ressourcesArray
-
-  // resources = [
-  //   { id : 1, name : 'Dan Stevenson' },
-  //   { id : 2, name : 'Talisha Babin' }
-  // ];
-
-  events = this.rowsArray
-
-  // events = [
-  //   { "id" : 1,
-  //     "startDate" : "2022-01-01 09:28:04",
-  //     "duration" : 3,
-  //     "durationUnit" : "d",
-  //     "name" :"Event 1"
-  //   },
-  //   { "id" : 2,
-  //     "startDate" : "2022-01-01 09:59:50",
-  //     "endDate" : "2022-01-01 10:00:06",
-  //     "name" : "Event 2",
-  //     "eventColor" : "violet"
-  //   }
-  // ];
-
-  assignments = this.assignementArray
-
-  // assignments = [
-  //   { event : 1, resource : 1 },
-  //   { event : 2, resource : 2 }
-  // ];
-
-  // dependencies = [
-  //   { fromEvent : 1, toEvent : 2 }
-  // ];
+  resources = this.resourcesArray;
+  events = this.rowsArray;
+  assignments = this.assignmentsArray;
 
   schedulerProConfig = schedulerProConfig;
   projectConfig = projectConfig;
